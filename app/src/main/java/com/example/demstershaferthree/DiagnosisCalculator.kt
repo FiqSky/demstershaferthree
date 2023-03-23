a784c7de36c8d96d9535cfeaed9f2aad63abb638
fiqsky <fiqihfm05@gmail.com>
2023-03-24 00:22:55 +0700

    implement metode v2

---
+++ app/src/main/java/com/example/demstershaferthree/DiagnosisCalculator.kt
@@ -0,0 +1,121 @@
+package com.example.demstershaferthree
+
+import android.content.ContentValues.TAG
+import android.util.Log
+import com.google.firebase.database.*
+
+class DiagnosisCalculator(private val databaseRef: DatabaseReference) {
+    fun calculate(selectedGejala: List<String>, listener: DiagnosisListener) {
+        // Inisialisasi variabel
+        val daftarPenyakit = mutableListOf<String>()
+        val daftarBelief = mutableMapOf<String, Double>()
+
+        // Looping untuk mendapatkan daftar penyakit dan faktor keyakinannya
+        databaseRef.child("PENYAKIT").addListenerForSingleValueEvent(object : ValueEventListener {
+            override fun onDataChange(snapshot: DataSnapshot) {
+                for (penyakitSnapshot in snapshot.children) {
+                    val kode_penyakit =
+                        penyakitSnapshot.child("kode_penyakit").getValue(String::class.java)
+                    val daftar_gejala = penyakitSnapshot.child("daftar_gejala")
+                        .getValue(object : GenericTypeIndicator<List<String>>() {})
+                    var belief = 1.0
+                    for (gejala in selectedGejala) {
+                        if (daftar_gejala?.contains(getKodeGejala(gejala)) == true) {
+                            belief *= getBobotGejala(gejala)
+                        } else {
+                            belief *= (1 - getBobotGejala(gejala))
+                        }
+                    }
+                    daftarPenyakit.add(kode_penyakit!!)
+                    daftarBelief[kode_penyakit] = belief
+                }
+
+                // Menghitung faktor keyakinan total
+                var beliefTotal = 0.0
+                for (belief in daftarBelief.values) {
+                    beliefTotal += belief
+                }
+
+                // Menghitung faktor keyakinan akhir
+                val daftarBeliefAkhir = mutableMapOf<String, Double>()
+                for (kode_penyakit in daftarPenyakit) {
+                    val beliefAkhir =
+                        daftarBelief[kode_penyakit]!! / (beliefTotal - daftarBelief[kode_penyakit]!!)
+                    daftarBeliefAkhir[kode_penyakit] = beliefAkhir
+                }
+
+                // Mengembalikan hasil diagnosis melalui listener
+                listener.onDiagnosisComplete(daftarBeliefAkhir)
+                Log.d(TAG, "onDaftarBeliefAkhir: $daftarBeliefAkhir")
+            }
+
+            override fun onCancelled(error: DatabaseError) {
+                // Handle database error
+                listener.onDiagnosisError(error.message)
+            }
+        })
+    }
+
+    // Fungsi untuk mendapatkan bobot gejala dari Firebase Realtime Database
+    private fun getBobotGejala(namaGejala: String): Double {
+        var bobot = 0.0
+        databaseRef.child("GEJALA").orderByChild("gejala").equalTo(namaGejala)
+            .addListenerForSingleValueEvent(object : ValueEventListener {
+                override fun onDataChange(snapshot: DataSnapshot) {
+                    if (snapshot.exists()) {
+                        for (gejalaSnapshot in snapshot.children) {
+                            bobot =
+                                gejalaSnapshot.child("bobot").getValue(Double::class.java) ?: 0.0
+                        }
+                    }
+                }
+
+                override fun onCancelled(error: DatabaseError) {
+                    // Handle database error
+                }
+            })
+        return bobot
+    }
+
+    // Fungsi untuk mendapatkan kode gejala dari Firebase Realtime Database
+    private fun getKodeGejala(namaGejala: String): String {
+        var kode = ""
+        databaseRef.child("GEJALA").orderByChild("gejala").equalTo(namaGejala)
+            .addListenerForSingleValueEvent(object : ValueEventListener {
+                override fun onDataChange(snapshot: DataSnapshot) {
+                    if (snapshot.exists()) {
+                        for (gejalaSnapshot in snapshot.children) {
+                            kode = gejalaSnapshot.child("kode_gejala").getValue(String::class.java)
+                                ?: ""
+                        }
+                    }
+                }
+
+                override fun onCancelled(error: DatabaseError) {
+                    // Handle database error
+                }
+            })
+        return kode
+    }
+
+    // Fungsi untuk mendapatkan nama penyakit dari Firebase Realtime Database
+    fun getNamaPenyakit(kodePenyakit: String): String {
+        var nama = ""
+        databaseRef.child("PENYAKIT").orderByChild("kode_penyakit").equalTo(kodePenyakit)
+            .addListenerForSingleValueEvent(object : ValueEventListener {
+                override fun onDataChange(snapshot: DataSnapshot) {
+                    if (snapshot.exists()) {
+                        for (penyakitSnapshot in snapshot.children) {
+                            nama = penyakitSnapshot.child("nama_penyakit").getValue(String::class.java) ?: ""
+                        }
+                    }
+                }
+
+                override fun onCancelled(error: DatabaseError) {
+                    // Handle database error
+                }
+            })
+        return nama
+    }
+
+}
--- app/src/main/java/com/example/demstershaferthree/SymptomActivity.kt
+++ app/src/main/java/com/example/demstershaferthree/SymptomActivity.kt
@@ -1,9 +1,12 @@
 package com.example.demstershaferthree
 
+import android.content.ContentValues.TAG
 import android.os.Bundle
+import android.util.Log
 import android.view.View
 import android.widget.ArrayAdapter
 import android.widget.ListView
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.google.firebase.database.*
 
@@ -51,8 +54,28 @@ class SymptomActivity : AppCompatActivity() {
             val position = checkedItems.keyAt(i)
             if (checkedItems.valueAt(i)) {
                 selectedGejala.add(gejalaList[position])
+                Log.d(TAG, "onButtonClick: $selectedGejala")
             }
         }
-        // Do something with selectedGejala
+
+        val diagnosisCalculator = DiagnosisCalculator(databaseRef)
+        diagnosisCalculator.calculate(selectedGejala, object : DiagnosisListener {
+            override fun onDiagnosisComplete(daftarBeliefAkhir: Map<String, Double>) {
+                // Menampilkan hasil diagnosis
+                var hasilDiagnosis = ""
+                for (kode_penyakit in daftarBeliefAkhir.keys) {
+                    if (daftarBeliefAkhir[kode_penyakit]!! >= 0.5) {
+                        hasilDiagnosis += "${diagnosisCalculator.getNamaPenyakit(kode_penyakit)} (${daftarBeliefAkhir[kode_penyakit]})\n"
+                    }
+                }
+                Toast.makeText(this@SymptomActivity, hasilDiagnosis, Toast.LENGTH_LONG).show()
+                Log.d(TAG, "onDiagnosisComplete: $hasilDiagnosis")
+            }
+
+            override fun onDiagnosisError(errorMessage: String?) {
+                // Menampilkan error jika terjadi kesalahan pada perhitungan
+                Toast.makeText(this@SymptomActivity, errorMessage, Toast.LENGTH_LONG).show()
+            }
+        })
     }
 }
