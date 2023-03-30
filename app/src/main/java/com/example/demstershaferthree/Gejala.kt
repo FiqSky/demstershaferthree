4c891fc8dd6f3adaf10c377b1139825079760be7
fiqsky <fiqihfm05@gmail.com>
2023-03-30 19:31:12 +0700

    implement metode v15 clear branches

--- app/src/main/java/com/example/demstershaferthree/Gejala.kt
+++
@@ -1,8 +0,0 @@
-package com.example.demstershaferthree
-
-data class Gejala(
-    val kodeGejala: String = "",
-    val gejala: String = "",
-    val bobot: Double = 0.0,
-    var isSelected: Boolean = false
-)
--- app/src/main/java/com/example/demstershaferthree/Penyakit.kt
+++
@@ -1,7 +0,0 @@
-package com.example.demstershaferthree
-
-data class Penyakit(
-    val kode_penyakit: String?,
-    val nama_penyakit: String?,
-    val daftar_gejala: List<String>
-)
